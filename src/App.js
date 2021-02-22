b308e716b511120d2f5aab549dfacd97e2c320f4
Mihir240 <mihirr240@gmail.com>
2021-02-22 02:03:14 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import './Board.css';
+import { ListItem }  from './ListItem.js';
+import { Board }  from './Board.js';
+import { useState, useRef } from 'react';
+
+
+
 
 function App() {
+  
+  
+  const [board, setBoard] = useState(
+      ['','','','','','','','','']
+    );
+  
+  
+  
+
+  // const state = useState([]);
+  const [myList, changeList ] = useState([]); //destructuring an array
+  // pass in one arg to usestate which is our intial state value
+  //each time state changes, whole compenent will rerender
+  const inputRef = useRef(null);
+  
+  function onClickButton(){
+    // myList.push('New item added');
+    // const newList = [...myList, 'New item added'];
+    const userText = inputRef.current.value;
+    changeList(prevList => [...prevList, userText]);
+  }
+  
+  export function updateBoard(arrIndex, value){
+    
+    setBoard(prevBoard => prevBoard.map( (elem,index) => {
+          if (index == arrIndex)
+              return value;
+          else
+              return elem;
+      
+    }) );
+    
+  }
+  
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Board item={ updateBoard }  />
+    <h1>My Favourites</h1>
+    <input ref={inputRef} type="text" />
+    <button onClick={onClickButton}> Add a list </button>
+      <ul>  
+        {myList.map(item => <ListItem name={item} />)};
+        
+      </ul>
+    
+    
     </div>
   );
 }
 
 export default App;
+
+export { updateBoard };
+
+
+
+
+
