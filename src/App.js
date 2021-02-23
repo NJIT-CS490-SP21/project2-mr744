14a822474534eb30c7136d0240fbd9eca1d831ee
Mihir240 <mihirr240@gmail.com>
2021-02-23 03:35:09 +0000

    update repo for hw10

--- src/App.js
+++ src/App.js
@@ -1,70 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import './Board.css';
-import { ListItem }  from './ListItem.js';
-import { Board }  from './Board.js';
-import { useState, useRef } from 'react';
+import { ListItem } from './ListItem.js';
+import { useState, useRef, useEffect } from 'react';
+import io from 'socket.io-client';
 
-
-
+//connects to the server that is running
+const socket = io(); // Connects to socket connection
 
 function App() {
-  
-  
-  const [board, setBoard] = useState(
-      ['','','','','','','','','']
-    );
-  
-  
-  
+  const [messages, setMessages] = useState([]); // State variable, list of messages
+  const inputRef = useRef(null); // Reference to <input> element
 
-  // const state = useState([]);
-  const [myList, changeList ] = useState([]); //destructuring an array
-  // pass in one arg to usestate which is our intial state value
-  //each time state changes, whole compenent will rerender
-  const inputRef = useRef(null);
-  
-  function onClickButton(){
-    // myList.push('New item added');
-    // const newList = [...myList, 'New item added'];
-    const userText = inputRef.current.value;
-    changeList(prevList => [...prevList, userText]);
+  function onClickButton() {
+    if (inputRef != null) {
+      const message = inputRef.current.value;
+      // If your own client sends a message, we add it to the list of messages to 
+      // render it on the UI.
+      setMessages(prevMessages => [...prevMessages, message]);
+      socket.emit('chat', { message: message });
+    }
   }
-  
-  export function updateBoard(arrIndex, value){
-    
-    setBoard(prevBoard => prevBoard.map( (elem,index) => {
-          if (index == arrIndex)
-              return value;
-          else
-              return elem;
-      
-    }) );
-    
-  }
-  
-   
+
+  // The function inside useEffect is only run whenever any variable in the array
+  // (passed as the second arg to useEffect) changes. Since this array is empty
+  // here, then the function will only run once at the very beginning of mounting.
+  useEffect(() => {
+    // Listening for a chat event emitted by the server. If received, we
+    // run the code in the function that is passed in as the second arg
+    socket.on('chat', (data) => {
+      console.log('Chat event received!');
+      console.log(data);
+      // If the server sends a message (on behalf of another client), then we
+      // add it to the list of messages to render it on the UI.
+      setMessages(prevMessages => [...prevMessages, data.message]);
+    });
+  }, []);
+
   return (
     <div>
-    <Board item={ updateBoard }  />
-    <h1>My Favourites</h1>
-    <input ref={inputRef} type="text" />
-    <button onClick={onClickButton}> Add a list </button>
-      <ul>  
-        {myList.map(item => <ListItem name={item} />)};
-        
+      <h1>Chat Messages</h1>
+      Enter message here: <input ref={inputRef} type="text" />
+      <button onClick={onClickButton}>Send</button>
+      <ul>
+        {messages.map((item, index) => <ListItem key={index} name={item} />)}
       </ul>
-    
-    
     </div>
   );
 }
 
 export default App;
-
-export { updateBoard };
-
-
-
-
-
--- src/Board.js
+++ src/Board.js
@@ -1,61 +1,17 @@
 import React from 'react';
-import { updateBoard } from './App.js';
-
-
-
-
-// export function addPiece(){
-//   //update the board data structure
-  
-  
-//   //update the UI 
-//   const itemRef = useRef(null);
-  
-  
-  
-  
-// }
-
+import { Box } from './Box.js';
 
 export function Board(props){
-    
-    
-      // const all_divs = [];
-      // for(let i=0 ; i<9; i++){
-      //   //   let str = `box ${i}`;
-      //     all_divs.push(<div onClick={ addPiece } class={`box box${i}`} ></div>)
-      // }
-   
-      
-      // for(let i=0; i<3; i++){
-      //   for(let j=0; j<3; j++)
-      //     all_divs.push(<div onClick={ () => props.addPiece('X') } class={`box box${i}`} > {props.item}</div>);
-      // }
-    
-    
-    
-            //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
-        //   <div class="box"></div>
+
     
     return (
        <div class="board">
 
-              <div onClick= {updateBoard(0,'X')} class="box"></div>
-              <div onClick= {updateBoard(1,'X')} class="box"></div>
-              <div onClick= {updateBoard(2,'X')} class="box"></div>
-              <div onClick= {updateBoard(3,'X')} class="box"></div>
-              <div onClick= {updateBoard(4,'X')} class="box"></div>
-              <div onClick= {updateBoard(5,'X')} class="box"></div>
-              <div onClick= {updateBoard(6,'X')} class="box"></div>
-              <div onClick= {updateBoard(7,'X')} class="box"></div>
-              <div onClick= {updateBoard(8,'X')} class="box"></div>
+              { props.board.map((elem,index)=> {  
+                  
+                  return <Box key={index} index={ index } updateBoard={props.updateBoard} board={props.board} />
+                  
+              }) }
        
         </div>
         
