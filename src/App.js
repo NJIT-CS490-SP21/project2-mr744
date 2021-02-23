8dcc7b85fdb446537d822c0e34c12c14bfcadbdf
Mihir240 <mihirr240@gmail.com>
2021-02-23 14:51:36 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import './Board.css';
 import { ListItem }  from './ListItem.js';
 import { Board }  from './Board.js';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import io from 'socket.io-client';
 
 
-// export { updateBoard } ;
+const socket = io();
 
 function App() {
   
@@ -26,8 +27,31 @@ function App() {
                 return elem;
         
       }) );
-   
+      
+      socket.emit('move', {arrIndex: arrIndex, boardVal: value});
+      
+
   }
+  
+  //define once and then always listening
+  //always put listeners in useEffect
+  useEffect(() =>{
+    
+    //setup the data object in python file
+    socket.on('move', (data) =>{
+      
+      setBoard(prevBoard => prevBoard.map( (elem,index) => {
+    
+        if (index == data.arrIndex)
+            return data.boardVal;
+        else
+            return elem;
+    
+      }) );
+      
+    });
+    
+  }, []);
 
   return (
     <div>
