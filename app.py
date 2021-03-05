870ddf40de2eb9b42e8a72e46c1b12362ce19c06
Mihir240 <mihirr240@gmail.com>
2021-03-05 23:55:52 +0000

    setup db connection for player insertion upon joining the game

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import join_room, leave_room
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__, static_folder='./build/static')
+
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+import models
+db.create_all()
 
 
 cors = CORS(app, resources={r"/": {"origins": ""}})
@@ -62,9 +76,17 @@ def on_chat(data): # data is whatever arg you pass in your emit call on client
 @socketio.on('login')
 def on_players(data):
     global id_count
-
-    isActive = False
+    #check if username is already in database, if so no need to add them again
+    #first query whole table
+    query_player = models.Players.query.filter_by(username=data['username']).first()
+    print(query_player)
+    #player not in db
+    if query_player == None:
+        new_player = models.Players(username=data['username'], score=100)
+        db.session.add(new_player)
+        db.session.commit()
     
+    isActive = False
     #the first player to join will be able to play again
     if len(users_list) == 0:
         join_room("able")
@@ -82,6 +104,8 @@ def on_players(data):
     join_room(room)
 
     data ={'user_dict': user_dict, 'users': users_list}
+    
+    print(models.Players.query.all())
 
     socketio.emit('login', data, broadcast=True, include_self=True, room=room)
     
@@ -111,9 +135,12 @@ def reset(data):
     socketio.emit('replay', data, broadcast=True, include_self=False)
     
 
+
+# Note we need to add this line so we can import app in the python shell
+if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-)
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    )
